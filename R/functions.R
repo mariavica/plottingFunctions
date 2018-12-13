4bfdc7144481fceecde96bb796ded8f2b46d8ab3
mariavica <maria.vila.cs@gmail.com>
2018-12-13 11:51:42 +0100

    More modifications

--- R/functions.R
+++ R/functions.R
@@ -1,16 +1,21 @@
 
+
 plotTwoTrends <- function(x1, x2, x = NA, 
-                          col1 = 'blue', col2 = 'red', lwd = 3,
+                          col1 = 'red', col2 = 'blue', lwd = 3,
                           normalization = 'none', norm_independent = FALSE,
                           expand = 0.25, fix.ylim = NULL,
                           loess = FALSE, sp = 0.5, res.x = 1000, 
-                          plot.sd = TRUE, 
-                          xlab = NA, ylab1 = NA, ylab2 = NA, omit.lab2 = FALSE ) {
+                          plot.sd = TRUE, error.plot = 0.95,
+                          xlab = NA, ylab1 = NA, ylab2 = NA, omit.lab2 = FALSE,
+                          main = NULL, pos.leg='topright', name.1=NULL, name.2,NULL) {
 
 
   if (ncol(x1)!=ncol(x2)) {
     stop('X1 and X2 have different number of columns')
   }
+  
+  error.plot <- qnorm(1-(1-error.plot)/2)
+  
   
   if (is.na(x)) { x <- 1:ncol(x1) }
  
@@ -27,29 +32,30 @@ plotTwoTrends <- function(x1, x2, x = NA,
     
     lo <- loess(y1~y1.x,span=sp)
  
-    sds<-loess.sd(y1~y1.x, nsigma = 1, span=sp)
 
     xl <- seq(from = min(y1.x), to= max(y1.x), length.out = res.x)
     yl <- predict(lo,xl)
     
     
     if (is.null(fix.ylim)) {
-      ylim.toplot <-c(min (yl) -(max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
+      fix.ylim <-c(min (yl) -(max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
     } 
-    
+  
 
-    plot(xl, yl , col=col1, lwd=lwd, type='l', main=NULL, xaxt='n',ylab='', xlab='',
+    plot(xl, yl , col=col1, lwd=lwd, type='l', main=main, axes=FALSE, xaxt='n',ylab='', xlab='',
          ylim=fix.ylim)
     
     if (is.na(ylab1)) {ylab1 <- colnames(x1)}
   
     axis(side = 1 , at=1:ncol(x1), labels=ylab1)
- 
+    axis(side = 2, col=col1)
+    
+    
     if (plot.sd) {
       
       lox<- predict(loess(y1~y1.x,span=sp), newdata = xl ,se=TRUE)
-      upper<- lox$fit + 2*lox$se.fit
-      lower<- lox$fit - 2*lox$se.fit
+      upper<- lox$fit + error.plot*lox$se.fit
+      lower<- lox$fit - error.plot*lox$se.fit
 
       polygon( x = c(xl, rev(xl)), y=c(upper,rev(lower)),
                col=rgb(red=1,blue=0,green=0,alpha=0.25), border=NA)
@@ -59,8 +65,34 @@ plotTwoTrends <- function(x1, x2, x = NA,
         
   } else {
     
+    #regular plot
     
-    plot()
+    yl <- tapply(y1,factor(y1.x), FUN=mean)
+    xl <- as.numeric(unique(factor(y1.x)))
+
+    if (is.null(fix.ylim)) {
+      fix.ylim <-c(min (yl) -(max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
+    } 
+    
+    plot(xl, yl , col=col1, lwd=lwd, type='l', main=main, axes=FALSE, xaxt='n',ylab='', xlab='',
+         ylim=fix.ylim)
+    
+    if (is.na(ylab1)) {ylab1 <- colnames(x1)}
+    
+    axis(side = 1 , at=1:ncol(x1), labels=ylab1)
+    axis(side = 2, col=col1)
+    
+    
+    if (plot.sd) {
+      
+      yl.sd <- tapply(y1,y1.x, FUN=sd)
+      upper<- yl + error.plot*yl.sd
+      lower<- yl - error.plot*yl.sd
+      
+      polygon( x = c(xl, rev(xl)), y=c(upper,rev(lower)),
+               col=rgb(red=1,blue=0,green=0,alpha=0.25), border=NA)
+      
+    }
     
     
   }
@@ -68,159 +100,128 @@ plotTwoTrends <- function(x1, x2, x = NA,
 
  
   
+  ## second plot
   
+  y2 <- as.vector(unlist(x2))
+  y2.x <- rep(x,each=nrow(x2))
   
-  xl <- seq(min(hic.x),max(hic.x), (max(hic.x) - min(hic.x))/1000)
-  yl <- predict(lo,xl)
-  plot(xl, yl , col='red', lwd=3, type='l', main=NULL, xaxt='n',ylab='', xlab='',
-       ylim=c(min (yl) - (max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
-       #ylim=c(-0.06, 0.04)
-  )
-  
-  
-  time.2<-seq(from=1,to=6,length.out = 1000)
-  
-  
-  lox<- predict(loess(hic.y~hic.x,span=sp), newdata = time.2 ,se=TRUE)
-  upper<- lox$fit + 2*lox$se.fit
-  lower<- lox$fit - 2*lox$se.fit
-  
-  
-  
-  polygon( x = c(time.2, rev(time.2)), y=c(upper,rev(lower)), col=rgb(red=1,blue=0,green=0,alpha=0.25), border=NA)
-  
-  sel<-to.cluster[which(all$cluster==j),1:12]  #check
-  hic.cor.y<-apply( sel, 2, mean)
-  hic.loess.y<-lox$fit
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
- 
- 
-  
-  
-  
-  time<-1:6
-  
-  hic.y<-as.vector(unlist(to.cluster[which(all$cluster==j),1:12]))    ### check
-  hic.x<-rep(rep(time,each=2),each=length(which(all$cluster==j)))          ### check
-  
-  lo <- loess(hic.y~hic.x,span=sp)
-  
-  
-  sds<-loess.sd(hic.y~hic.x, nsigma = 1, span=sp)
-  
-  #plot(x,y)
-  xl <- seq(min(hic.x),max(hic.x), (max(hic.x) - min(hic.x))/1000)
-  yl <- predict(lo,xl)
-  plot(xl, yl , col='red', lwd=3, type='l', main=NULL, xaxt='n',ylab='', xlab='',
-       ylim=c(min (yl) - (max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
-       #ylim=c(-0.06, 0.04)
-  )
-  
-  axis(side = 1 , at=1:6, labels=c("0h","1h","3h","6h","18h","120h"))
-  
-  time.2<-seq(from=1,to=6,length.out = 1000)
-  
-  
-  lox<- predict(loess(hic.y~hic.x,span=sp), newdata = time.2 ,se=TRUE)
-  upper<- lox$fit + 2*lox$se.fit
-  lower<- lox$fit - 2*lox$se.fit
-  
-  
-  
-  polygon( x = c(time.2, rev(time.2)), y=c(upper,rev(lower)), col=rgb(red=1,blue=0,green=0,alpha=0.25), border=NA)
-  
-  sel<-to.cluster[which(all$cluster==j),1:12]  #check
-  hic.cor.y<-apply( sel, 2, mean)
-  hic.loess.y<-lox$fit
-  
-  
-  ## rna
-  
-  rna.y<-as.vector(unlist(to.cluster[which(all$cluster==j),13:24]))  #check
-  rna.x<-rep(rep(time,2),each=length(which(all$cluster==j)))       #check
-  
-  
-  par(new = TRUE)
-  
-  lo <- loess(rna.y~rna.x,span=sp)
-  #plot(x,y)
-  xl <- seq(min(rna.x),max(rna.x), (max(rna.x) - min(rna.x))/1000)
-  yl <- predict(lo,xl)
-  plot(xl, yl, col='blue', lwd=3, type='l',axes=FALSE,xaxt='n',ylab='',
-       ylim=c(min (yl) - (max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
-       #ylim=c(-0.25,1)
-  )
-  if (i %in% good) { 
+  if (loess) {
+    if (length(y2)>10^7) {
+      warning('X2 too large, taking 10^7 random numbers')
+      sel <- sample(1:length(y2),10^7)
+      y2 <- y2[sel]
+      y2.x <- y2.x[sel]
+    }
+    
+    lo <- loess(y2~y2.x,span=sp)
+    
+    xl <- seq(from = min(y2.x), to= max(y2.x), length.out = res.x)
+    yl <- predict(lo,xl)
     
     
-    lox<- predict(lo, newdata = time.2 ,se=TRUE)
-    upper<- lox$fit + 2*lox$se.fit
-    lower<- lox$fit - 2*lox$se.fit
+    if (is.null(fix.ylim)) {
+      fix.ylim <-c(min (yl) -(max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
+    } 
     
-    time.2<-seq(from=1,to=11,length.out = 1000)
+    par(new = TRUE)
+
+    plot(xl, yl , col=col2, lwd=lwd, type='l', main=NULL, axes=FALSE, xaxt='n',ylab='', xlab='',
+         ylim=fix.ylim)
+    
+    if (is.na(ylab2)) {ylab2 <- colnames(x2)}
+    
+    axis(side = 4, col=col2)
+    
+    if (plot.sd) {
+      
+      lox<- predict(loess(y2~y2.x,span=sp), newdata = xl ,se=TRUE)
+      upper<- lox$fit + error.plot*lox$se.fit
+      lower<- lox$fit - error.plot*lox$se.fit
+      
+      polygon( x = c(xl, rev(xl)), y=c(upper,rev(lower)),
+               col=rgb(red=0,blue=1,green=0,alpha=0.25), border=NA)
+      
+    }
+    
+
+  } else {
+    
+    # regular plot
+    yl <- tapply(y2,factor(y2.x), FUN=mean)
+    xl <- as.numeric(unique(factor(y2.x)))
+    
+    if (is.null(fix.ylim)) {
+      fix.ylim <-c(min (yl) -(max(yl)-min(yl))*expand , max(yl) + (max(yl)-min(yl))*expand )
+    } 
+    
+    par(new = TRUE)
+    
+    plot(xl, yl , col=col2, lwd=lwd, type='l', main=NULL, axes=FALSE, xaxt='n',ylab='', xlab='',
+         ylim=fix.ylim)
+    
+    if (is.na(ylab2)) {ylab2 <- colnames(x2)}
+    
+    axis(side = 4, col=col2)
+    
+    if (plot.sd) {
+      
+      yl.sd <- tapply(y2, y2.x, FUN=sd)
+      upper<- yl + error.plot*yl.sd
+      lower<- yl - error.plot*yl.sd
+      
+      polygon( x = c(xl, rev(xl)), y=c(upper,rev(lower)),
+               col=rgb(red=0,blue=1,green=0,alpha=0.25), border=NA)
+      
+    }
     
     
-    polygon( x = c(time.2, rev(time.2)), y=c(upper,rev(lower)), col=rgb(red=0,blue=1,green=0,alpha=0.25), border=NA)
+    
     
   }
-  sel<-to.cluster[which(all$cluster==j),13:24]   ### check
-  rna.cor.y<-apply( sel, 2, mean)
-  rna.loess.y<-lox$fit
   
   
-  cor.data[j]<-cor(hic.cor.y,rna.cor.y, method = 'spearman')
-  p.data[j]<-cor.test(hic.cor.y,rna.cor.y, method = 'spearman')$p.val
-  cor.loess[j]<-cor(hic.loess.y,rna.loess.y, method = 'spearman')
-  p.loess[j]<-cor.test(hic.loess.y,rna.loess.y, method = 'spearman')$p.val
+  ### Legend
   
+  if(is.null(name.1)) deparse(substitute(x1))
+  if(is.null(name.2)) deparse(substitute(x2))
   
-  title(main=paste( "Cluster ",j," (cor=",round(cor.data[j],3),', p=',round(p.data[j],3),')',sep="")  )
-  legend("bottomright",c("WT","R35A"),col=c("red","blue"),lwd=3)     ##### check
-  
-  axis(side = 4)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+  legend(pos.leg, c(name.1,name.2), col=c(col1,col2), lwd=2)
   
   
   
   
 }
+
+  
+  
+ # a <- matrix(rnorm(1000), ncol=10, nrow = 100)
+#  b <- matrix(rnorm(1000), ncol=10, nrow = 100)
+  
+ # plotTwoTrends(x1 = a, x2 = b , loess = TRUE , expand = 0.75)
+  
+  plotTwoTrends(x1 = a, x2 = b , loess = TRUE , expand = 0.75, main = 'Juju')
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
